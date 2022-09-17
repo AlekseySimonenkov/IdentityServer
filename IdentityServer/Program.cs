339cf6e20992def72ed84f81351b42ec49fc22d1
AlekseySimonenkov <alexey.simonenkov.1999@mail.ru>
2022-09-18 01:10:13 +0300

    Добавьте файлы проекта.

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,20 @@
+using IdentityServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiResources(IdentityConfiguration.ApiResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddTestUsers(IdentityConfiguration.TestUsers)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseIdentityServer();
+
+app.MapGet("/", () => "Lilufar PIDOR");
+
+app.Run();
